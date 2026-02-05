52290c1b0ba917a2ceffb2a84c74317a14a56e4f
SuryaSekharNag <suryanag7431@gmail.com>
2026-02-06 00:03:03 +0530

    Updated portfolio UI, hero section, animations, navbar, projects, skills

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-window.addEventListener('scroll', function() {
+// ===== Scroll Reveal Animation =====
+window.addEventListener('scroll', function () {
     const reveals = document.querySelectorAll('.reveal');
 
     reveals.forEach(reveal => {
         const windowHeight = window.innerHeight;
         const elementTop = reveal.getBoundingClientRect().top;
 
-        if(elementTop < windowHeight - 150){
+        if (elementTop < windowHeight - 150) {
             reveal.classList.add('active');
         }
     });
 });
+
+
+// ===== Typing Animation in Hero Section =====
+const text = "Hello, I'm Surya ";
+const typingElement = document.getElementById("typing-text");
+
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        typingElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, 70);
+    }
+}
+
+window.addEventListener("load", typeEffect);
